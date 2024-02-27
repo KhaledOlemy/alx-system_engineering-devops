646bbe2164db5596f327b72a421d096744924bcb
khaledolemy <khaledolemy@gmail.com>
2024-02-27 21:22:38 +0200

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
-#Install nginx web server
+# This script does the following:
+#   1. updates packages
+#   2. installs Nginx
+#   3. sets listening port on 80
+#   4. sets `Hello World!` as a default response
+#   5. adjusts redirect_me page to 301
+#   6. starts/restarts server after everything
 
 package { 'nginx':
-  ensure => installed,
-}
+  ensure => installed}
 
-file_line { 'aaaaa':
-  ensure => 'present',
+file_line { 'nginx_80_redirect':
+  ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
+  after  => 'listen 80 default_server;'
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=VZrDxD0Za9I permanent;'}
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => present,
+  content => 'Hello World!'}
 
-service { 'nginx':
+service { 'nginx-start':
   ensure  => running,
-  require => Package['nginx'],
-}
+  require => Package['nginx']}
