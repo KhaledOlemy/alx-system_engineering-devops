60d60b809cd85cdf7a60f57e902a2dfef3ad14e9
khaledolemy <khaledolemy@gmail.com>
2024-02-27 21:26:28 +0200

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,41 +5,20 @@
 #   4. sets `Hello World!` as a default response
 #   5. adjusts redirect_me page to 301
 #   6. starts/restarts server after everything
-exec {'install-nginx':
-      provider => shell,
-      command  =>"""
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-sudo chmod -R 777 /var/www
-sudo echo 'Hello World!' > /var/www/html/index.nginx-debian.html
-sudo echo "Ceci n'est pas une page" > /var/www/html/404.html
-new_server_config=\
-"server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
 
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-	server_name olemy.tech;
+package { 'nginx':
+  ensure => installed}
 
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-	if (\$request_filename ~ redirect_me)
-	{
-		rewrite ^ https://www.youtube.com/watch?v=VZrDxD0Za9I permanent;
-	}
-	error_page 404 /404.html;
-}"
+file_line { 'nginx_80_redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;'
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=VZrDxD0Za9I permanent;'}
 
-sudo chmod -R 777 /etc/nginx/
-bash -c "echo -e '$new_server_config' > /etc/nginx/sites-enabled/default"
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => present,
+  content => 'Hello World!'}
 
-if [ "$(pgrep -c nginx)" -le 0 ];
-then
-	sudo service nginx start
-else
-	sudo service nginx restart
-fi
-"""
+service { 'nginx-start':
+  ensure  => running,
+  require => Package['nginx']}
