d83329a70b8be286dd78c8e75ec3fb12abcab0ae
khaledolemy <khaledolemy@gmail.com>
2024-03-04 15:32:50 +0200

    puppet_automation

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,35 @@
+# This script does the following:
+#   1. updates packages
+#   2. installs Nginx
+#   3. sets listening port on 80
+#   4. sets `Hello World!` as a default response
+#   5. adjusts redirect_me page to 301
+#   6. starts/restarts server after everything
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'nginx_redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=VZrDxD0Za9I permanent;',
+}
+
+file_line { 'nginx_header':
+  ensure => 'present',
+  path   => '/etc/nginx/nginx.conf',
+  after  => 'include /etc/nginx//sites-enabled/*;',
+  line   => 'add_header X-Served-By $HOSTNAME;',
+}
+
+
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
