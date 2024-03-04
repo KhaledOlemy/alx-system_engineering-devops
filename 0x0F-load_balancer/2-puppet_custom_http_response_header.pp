c1e42d106f79ddba377ee7283f3823b5905c00fd
khaledolemy <khaledolemy@gmail.com>
2024-03-04 15:37:54 +0200

    puppet_automation_fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,13 +17,12 @@ file_line { 'nginx_redirect':
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=VZrDxD0Za9I permanent;',
 }
 
-file_line { 'nginx_header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $HOSTNAME;',
+file_line { 'addHeader':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
 }
-
 
 file { '/var/www/html/index.nginx-debian.html':
   content => 'Hello World!',
