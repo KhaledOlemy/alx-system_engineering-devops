ea03fe94c474a496ad670f9b5eeeef3f16ca4a9e
khaledolemy <khaledolemy@gmail.com>
2024-03-25 23:23:01 +0200

    csv_fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,6 +5,7 @@ USAGE: 1-export_to_CSV.py employeeID
 """
 import requests
 import sys
+import time
 
 if __name__ == "__main__":
     employee_id = eval(sys.argv[1])
@@ -12,11 +13,19 @@ if __name__ == "__main__":
     emp_req = requests.get(f"{main_url}{employee_id}/todos").json()
     e_name = requests.get(f"{main_url}{employee_id}/").json().get('name')
     out_csv = ""
-    for task in emp_req:
-        tid = task['userId']
-        cp = task['completed']
-        tt = task['title']
-        out_csv += f""""{tid}","{e_name}","{cp}","{tt}" """.strip()
-        out_csv += "\n"
-    with open(f"{employee_id}.csv", "w") as csvfile:
-        csvfile.write(out_csv)
+    # for task in emp_req:
+    #     tid = task['userId']
+    #     cp = task['completed']
+    #     tt = task['title']
+    #     out_csv += f""""{tid}","{e_name}","{cp}","{tt}" """.strip()
+    #     out_csv += "\n"
+    #     l = line = '"' + str(task['userId']) + '","' + e_name + '","' + str(task['completed']) + '","' + task['title'] + '"'
+    # with open(f"{employee_id}.csv", "w") as csvfile:
+    #     csvfile.write(out_csv)
+    with open(f"{employee_id}.csv", "a") as csvfile:
+        for task in emp_req:
+            tid = task['userId']
+            cp = task['completed']
+            tt = task['title']
+            l = '"' + str(task['userId']) + '","' + e_name + '","' + str(task['completed']) + '","' + task['title'] + '"\n'
+            csvfile.write(l)
