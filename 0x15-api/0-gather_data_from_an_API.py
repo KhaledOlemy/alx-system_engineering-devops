dd50bb9a05165f27d26cd716ad4f3d6562ad0a56
khaledolemy <khaledolemy@gmail.com>
2024-03-25 21:47:13 +0200

    main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,15 +6,15 @@ USAGE: 0-gather_data_from_an_API.py employeeID
 import requests
 import sys
 
-
-employee_id = eval(sys.argv[1])
-main_url = "https://jsonplaceholder.typicode.com/users/"
-emp_req = requests.get(f"{main_url}{employee_id}/todos").json()
-e_name = requests.get(f"{main_url}{employee_id}/").json().get('name')
-n_tasks = len(emp_req)
-tasks_done = [i for i in emp_req if i.get('completed')]
-nd_tasks = len(tasks_done)
-e_statement = f"Employee {e_name} is done with tasks({nd_tasks}/{n_tasks}):"
-print(e_statement)
-for task in tasks_done:
-    print(f"\t {task.get('title')}")
+if __name__ == "__main__":
+    employee_id = eval(sys.argv[1])
+    main_url = "https://jsonplaceholder.typicode.com/users/"
+    emp_req = requests.get(f"{main_url}{employee_id}/todos").json()
+    e_name = requests.get(f"{main_url}{employee_id}/").json().get('name')
+    n_tasks = len(emp_req)
+    tasks_done = [i for i in emp_req if i.get('completed')]
+    nd_tasks = len(tasks_done)
+    e_statement = f"Employee {e_name} is done with tasks({nd_tasks}/{n_tasks}):"
+    print(e_statement)
+    for task in tasks_done:
+        print(f"\t {task.get('title')}")
